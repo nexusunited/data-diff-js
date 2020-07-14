db06fd831f0a08b6a910d5c1252bf68d5f38ba9d
Sebastian Felix Schwarz <schwarz@nexus-netsoft.de>
2020-07-14 16:50:33 +0200

    fix index.js for encoding param

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const {DiffParser} = require('./diff-parser')
 
-module.exports = (type, identifier, separator = null, debug = false) => new DiffParser(type, identifier, separator, debug);
+module.exports = (type, identifier, separator = null, encoding='utf8', debug = false) => new DiffParser(type, identifier, separator,encoding, debug);
